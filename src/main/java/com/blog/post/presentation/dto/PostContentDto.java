7b39fd7471846a01679e4b6068dff1d4122d8e02
lilloo04 <lilloo04@naver.com>
2025-04-08 15:06:53 +0900

    fix: Resolve content_type null error with @JsonProperty

--- src/main/java/com/blog/post/presentation/dto/PostContentDto.java
+++ src/main/java/com/blog/post/presentation/dto/PostContentDto.java
@@ -1,10 +1,13 @@
 package com.blog.post.presentation.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class PostContentDto {
     private String contentType;  // "text" or "image"
     private String content;
     private Integer order;
 
+    @JsonProperty("content_type")
     public String getContentType() { return contentType; }
     public void setContentType(String contentType) {
         this.contentType = contentType;
